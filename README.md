b04961a4e98d4337699653c5a5d7b60e2374af26
Chu Wang <cwang9@wpi.edu>
2018-11-27 12:24:00 -0500

    comment out the markers for now

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # CS539-Project
+
+#create virtual environment with conda
+conda create --name myEnv django
+
+#activate the environment
+activate myEnv
+
+#install Django
+pip install django
+
+#runserver
+python manage.py runserver
+
+#deactivate the environment
+deactivate myEnv
--- static/js/geocoding.js
+++ static/js/geocoding.js
@@ -3,31 +3,47 @@ sample code from
 https://developers.google.com/maps/documentation/javascript/geocoding#GetStarted
 */
 
-var geocoder; //To use later
-var map; //Your map
-
+//initialize map
 function initialize() {
-  geocoder = new google.maps.Geocoder();
-  //Default setup
-  var latlng = new google.maps.LatLng(39.5, -98.35);
-  var mapOptions = {
-    zoom: 4,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  map = new google.maps.Map(document.getElementById("starbucksmap"), mapOptions);
+    var latlng = new google.maps.LatLng(39.5, -98.35);
+    var mapOptions = {
+        zoom: 4,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(document.getElementById("starbucksmap"), mapOptions);
+
+    var geocoder = new google.maps.Geocoder();
+
+    //var button = document.getElementById("floating-panel");
+    var button = document.getElementById("searchButton");
+    button.addEventListener('click', function() {codeAddress(geocoder, map);
+        });
+    if(button == null){
+        alert("Not Initialized");
+    }
+
 }
 
+
+
+//re-center the map according to the zipcode input
 //Call this wherever needed to actually handle the display
-function codeAddress(zipCode) {
-    geocoder.geocode( { 'address': zipCode}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        //Got result, center the map and put it out there
+function codeAddress(geocoder, map) {
+    //get zipcode
+    var zipCode = document.getElementById('location').value;
+    //only search within USA
+    geocoder.geocode( { 'address': zipCode, "componentRestrictions":{"country":"USA"}},
+     function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        //zoom factor: 4 --> 8
+        map.setZoom(8);
         map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
+///TODO reset the map when a new zipcode/address is entered when implement markers
+//        var marker = new google.maps.Marker({
+//            map: map,
+//            position: results[0].geometry.location
+//        });
       } else {
         alert("Geocode was not successful for the following reason: " + status);
       }
